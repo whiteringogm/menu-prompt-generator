8691db35427f4997861195da9d868b6dc265feb0
whiteringogm <whiteringogm@gmail.com>
2026-07-27 21:11:58 +0900

    Refresh cache after pantry export fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'menu-prompt-generator-v5.1.4';
+const CACHE = 'menu-prompt-generator-v5.1.5';
 const ASSETS = ['./', './index.html', './app.js?v=510', './yesterday-ui.js?v=513', './pantry-export.js?v=514', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
