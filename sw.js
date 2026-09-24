e811b87eaa79ae97ae393d06902cde93f769be56
whiteringogm <whiteringogm@gmail.com>
2026-09-24 10:14:09 +0900

    Refresh cache for meal register button layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'menu-prompt-generator-v5.1.17-r2';
+const CACHE = 'menu-prompt-generator-v5.1.17-r3';
 const STOCK_CONTROLS = './stock-buttons.js?v=522';
 const STOCK_UI = './stock-ui.js?v=516';
 const PLAN_ORDER = './plan-order.js?v=516';
